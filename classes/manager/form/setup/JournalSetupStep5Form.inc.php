ff552aa9d4e0606161dcbd832f7269e17f4988a8
Peter Dietz <dietz.72@osu.edu>
2011-05-16 11:49:12 -0400

    Fixed upload css problem by allowing files detected as text/x-c

--- classes/manager/form/setup/JournalSetupStep5Form.inc.php
+++ classes/manager/form/setup/JournalSetupStep5Form.inc.php
@@ -209,7 +209,7 @@ class JournalSetupStep5Form extends JournalSetupForm {
 		$fileManager = new PublicFileManager();
 		if ($fileManager->uploadedFileExists($settingName)) {
 			$type = $fileManager->getUploadedFileType($settingName);
-			if ($type != 'text/plain' && $type != 'text/css') {
+			if ($type != 'text/plain' && $type != 'text/css' && $type != 'text/x-c') {
 				return false;
 			}
 
